b30af35a483d6b7a061e3085899d3a6ff9d7cf78
Przemysław Pliński <przemyslaw.plinski@itmagination.com>
2022-11-22 12:03:41 +0100

    update

--- conftest.py
+++ conftest.py
@@ -10,30 +10,30 @@ def init_driver():
     web_driver.close()
 
 
-def pytest_sessionfinish(session, exitstatus):
-    """
-    Called after whole test run finished, right before
-    returning the exit status to the system.
-    """
-    with open("stats.txt", 'w') as file:
-        itm = session.perform_collect()
-        file.write(str(session.testscollected) + " ")
-        file.write(str(itm) + " ")
-        file.write(str(session.testsfailed ) + " ")
-        file.write(str(itm[0] ) + " ")
-        file.write(str(itm[0].reportinfo() ) + " ")
+# def pytest_sessionfinish(session, exitstatus):
+#     """
+#     Called after whole test run finished, right before
+#     returning the exit status to the system.
+#     """
+#     with open("stats.txt", 'w') as file:
+#         itm = session.perform_collect()
+#         file.write(str(session.testscollected) + " ")
+#         file.write(str(itm) + " ")
+#         file.write(str(session.testsfailed ) + " ")
+#         file.write(str(itm[0] ) + " ")
+#         file.write(str(itm[0].reportinfo() ) + " ")
         
         
 
-def pytest_report_collectionfinish(config, start_path, startdir, items):
-    with open("stats.txt", 'w') as file:
-        file.write('Number of tests: ' + str(len(items)))
+# def pytest_report_collectionfinish(config, start_path, startdir, items):
+#     with open("stats.txt", 'w') as file:
+#         file.write('Number of tests: ' + str(len(items)))
 
-@pytest.hookimpl()
-def pytest_report_from_serializable(config, data):
-    with open("stats.txt", 'w') as file:
-        for i in data.keys():
-            file.write(i + " ")
+# @pytest.hookimpl()
+# def pytest_report_from_serializable(config, data):
+#     with open("stats.txt", 'w') as file:
+#         for i in data.keys():
+#             file.write(i + " ")
 
 
 
--- tests/test_contact_us.py
+++ tests/test_contact_us.py
@@ -7,6 +7,7 @@ scenarios("../features/frontend/contact_us.feature")
 
 
 @given("Open Contact Us")
-def open_contact_us(init_driver):
+def open_contact_us(init_driver, variables):
+    assert 'Firefox' == variables['capabilities']['browser']
     page = ContactUs(init_driver)
     page.go_to_contact_us()
